a02147317d3d968d6cf58cc55e1d72d1bf890a9c
Pihu Mohanty. <kumarimanjusrimohantycse2024@gmail.com>
2026-04-26 21:23:29 +0530

    Create PhishGuardApplication.java Spring Boot entry point

---
+++ backend/src/main/java/com/phishguard/PhishGuardApplication.java
@@ -0,0 +1,11 @@
+package com.phishguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhishGuardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PhishGuardApplication.class, args);
+    }
+}
