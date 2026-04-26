bc17a100f65cf90c70dc3368d9eba2d1af392536
Pihu Mohanty. <kumarimanjusrimohantycse2024@gmail.com>
2026-04-26 21:22:05 +0530

    Create src/index.js for React application

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PhishGuardApp from './PhishGuardApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render(<PhishGuardApp />);
